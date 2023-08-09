5fb5f63e70497bf7cc848c610c43d58e5715bcc1
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-08-09 10:59:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
+    // Write your algorithm here
+    const len = word.length;
+    for(let i = 0; i < len/2; i++){
+      if(word[i] !== word[len -1 -i]){
+        return false;
+      }
+    }
+    return true;
+  }
 /* 
   Add your pseudocode here
 */
@@ -20,6 +26,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("shawarma"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("lucy"));
 }
 
 module.exports = isPalindrome;
